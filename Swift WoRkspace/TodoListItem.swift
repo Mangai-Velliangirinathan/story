8fb39ea5750556e3de46cc34fa1cfee187964e84
mangai <mangaiyarkkarasi_velliangirinathan@thbs.com>
2020-07-23 14:51:00 +0530

    iosprojects

---
+++ Swift WoRkspace/TodoListItem.swift
@@ -0,0 +1,9 @@
+//
+//  TodoListItem.swift
+//  ToDoAPp
+//
+//  Created by Mangaiyarkkarasi_Velliangirinathan on 07/07/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import Foundation
---
+++ Swift WoRkspace/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Mangaiyarkkarasi_Velliangirinathan on 30/06/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
